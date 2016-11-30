6d362d419ffa3e8fa32dbb696baaa5584f81c531
Jackson Mkhabela <JacksonM@SAGOLDCOIN.local>
2016-11-30 16:23:26 +0200

    Changed added links to the budget table

--- views/budget.php
+++ views/budget.php
@@ -7,24 +7,31 @@ factory()->getInclusion("header")->Inclusion();
 
 $conn = con();
 
+?>
+    <a href="../views/createBudget.php">Create Budget</a>
+<?php
 $sql ="SELECT * FROM user_budgets WHERE user_id =".$_SESSION["user_id"];
 $result = mysqli_query($conn,$sql);
 if(mysqli_num_rows($result) > 0){
+    ?>
+
+    <br>
+    <table>
+        <tr>
+            <th>Budget</th>
+        </tr>
+    <?php
     while ($row = mysqli_fetch_assoc($result)){
-        echo $row["name"];
+        ?>
+        <tr>
+            <td><a href="#"><?php echo $row["name"]; ?></a></td>
+        </tr>
+        <?php
+        //echo $row["name"];
     }
 }
 
 ?>
-<a href="../views/createBudget.php">Create Budget</a>
-<br>
-<table>
 
-    <tr>
-        <td>Budget</td>
-    </tr>
-    <tr>
-        <td><a href="#">Family</a></td>
-    </tr>
 </table>
 <?php factory()->getInclusion("footer")->Inclusion();?>
