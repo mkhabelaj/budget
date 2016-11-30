4a7afbee63643e8c064cc6798439798dc48f2899
Jackson Mkhabela <JacksonM@SAGOLDCOIN.local>
2016-11-30 15:45:16 +0200

    added a database file

---
+++ database/databaseConnection.php
@@ -0,0 +1,14 @@
+<?php
+//$con = mysqli_connect("localhost","root","","budget");
+
+function con(){
+    return mysqli_connect("localhost","root","","budget");
+}
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  
+?>
