5f97fe7e2afafad1115e6d79ce586df376609da0
mkhabelaj <jmkhablea@gmail.com>
2017-01-25 14:21:23 +0200

    sql changes

--- database/budget.sql
+++ database/budget.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 25, 2017 at 01:07 PM
+-- Generation Time: Jan 25, 2017 at 01:20 PM
 -- Server version: 10.0.17-MariaDB
 -- PHP Version: 5.6.14
 
@@ -32,24 +32,6 @@ CREATE TABLE `budget_instance` (
   `description` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `budget_instance`
---
-
-INSERT INTO `budget_instance` (`budget_instance_id`, `name`, `description`) VALUES
-(108, 'test', ''),
-(109, 'test ', ''),
-(110, 'asfasf', ''),
-(113, 'Test', ''),
-(114, 'sddr', ''),
-(115, 'zfdgdfg', ''),
-(116, 'test', ''),
-(117, 'family', ''),
-(118, 'test', ''),
-(119, 'test', ''),
-(120, 'personal', ''),
-(121, 'test', '');
-
 -- --------------------------------------------------------
 
 --
@@ -62,45 +44,6 @@ CREATE TABLE `budget_instance_catagory` (
   `budget_Instance_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `budget_instance_catagory`
---
-
-INSERT INTO `budget_instance_catagory` (`budget_Instance_catagory_id`, `catagory_id`, `budget_Instance_id`) VALUES
-(52, 67, 108),
-(53, 68, 108),
-(54, 69, 108),
-(55, 70, 108),
-(59, 74, 108),
-(61, 68, 108),
-(63, 77, 108),
-(64, 78, 108),
-(69, 83, 108),
-(73, 86, 108),
-(74, 87, 108),
-(75, 88, 108),
-(76, 89, 109),
-(77, 90, 109),
-(78, 91, 109),
-(79, 92, 109),
-(80, 93, 109),
-(83, 96, 109),
-(86, 99, 109),
-(87, 100, 109),
-(88, 101, 109),
-(94, 107, 113),
-(95, 108, 113),
-(96, 109, 113),
-(97, 110, 113),
-(98, 111, 116),
-(99, 112, 117),
-(100, 113, 117),
-(101, 114, 117),
-(102, 115, 118),
-(103, 116, 119),
-(104, 117, 119),
-(105, 118, 119);
-
 -- --------------------------------------------------------
 
 --
@@ -112,47 +55,6 @@ CREATE TABLE `category` (
   `name` varchar(25) NOT NULL,
   `description` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
-
---
--- Dumping data for table `category`
---
-
-INSERT INTO `category` (`category_id`, `name`, `description`) VALUES
-(67, 'test 1', ''),
-(68, 'test 2', ''),
-(69, 'test 3', ''),
-(70, 'test 4', ''),
-(74, 'test two', ''),
-(77, 'test 90', ''),
-(78, 'the', ''),
-(83, 'rdu', ''),
-(86, 'test 200', ''),
-(87, 'okay', ''),
-(88, 'test ', ''),
-(89, 'this', ''),
-(90, 'xdfg', ''),
-(91, 'SDFSDFSDF', ''),
-(92, 'SDFSDFSSDF', ''),
-(93, 'test 4', ''),
-(96, 'tetet', ''),
-(99, 'sf', ''),
-(100, 'zcfg', ''),
-(101, 'tr', ''),
-(102, 'sam', ''),
-(103, 'ra', ''),
-(105, 'fuel', ''),
-(107, 'Test', ''),
-(108, 'grey', ''),
-(109, 'greys', ''),
-(110, 'greyst', ''),
-(111, 'fridge', ''),
-(112, 'grey', ''),
-(113, 'grey2', ''),
-(114, 'Gray', ''),
-(115, 'fuel', ''),
-(116, 'this', ''),
-(117, 'thiss', ''),
-(118, 'thisss', '');
 
 -- --------------------------------------------------------
 
@@ -168,55 +70,6 @@ CREATE TABLE `category_amounts` (
   `time_line_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `category_amounts`
---
-
-INSERT INTO `category_amounts` (`catergory_amount_id`, `actual_amount`, `projected_amount`, `catergory_id`, `time_line_id`) VALUES
-(87, 500.00, 500.00, 67, 138),
-(88, 500.00, 500.00, 68, 138),
-(89, 500.00, 500.00, 69, 138),
-(90, 500.00, 500.00, 70, 138),
-(91, 0.00, 500.00, 67, 139),
-(98, 0.00, 500.00, 74, 139),
-(102, 0.00, 500.00, 74, 140),
-(104, 40.00, 40.00, 68, 140),
-(106, 100.00, 4000.00, 77, 140),
-(107, 500.00, 500.00, 78, 140),
-(112, 456.00, 456.00, 83, 140),
-(116, 100.00, 100.00, 86, 140),
-(117, 55.00, 66.00, 87, 140),
-(118, 600.00, 500.00, 88, 140),
-(119, 600.00, 500.00, 89, 141),
-(120, 123123.00, 0.00, 90, 141),
-(121, 0.00, 0.00, 91, 141),
-(122, 20.00, 50.00, 92, 141),
-(123, 500.00, 500.00, 93, 141),
-(126, 500.00, 500.00, 96, 141),
-(129, 5.00, 5.00, 99, 141),
-(130, 7.00, 7.00, 100, 141),
-(132, 5.98, 5.90, 101, 141),
-(138, 45.00, 55.00, 107, 145),
-(139, 56.01, 90.05, 108, 145),
-(140, 56.02, 90.00, 109, 145),
-(141, 56.03, 90.00, 110, 145),
-(142, 5000.00, 10000.00, 111, 148),
-(143, 0.00, 55.00, 107, 149),
-(144, 0.00, 90.05, 108, 149),
-(145, 501.00, 90.00, 109, 149),
-(146, 10.00, 90.00, 110, 149),
-(147, 500.00, 500.00, 112, 150),
-(148, 500.00, 500.00, 113, 150),
-(149, 500.89, 500.00, 114, 150),
-(150, 80.00, 500.00, 112, 151),
-(151, 300.00, 500.00, 113, 151),
-(153, 0.00, 500.00, 112, 152),
-(154, 0.00, 500.00, 113, 152),
-(155, 500.00, 1000.00, 115, 153),
-(156, 500.00, 500.00, 116, 154),
-(157, 10000.00, 500.00, 117, 154),
-(158, 500.00, 5000.00, 118, 154);
-
 -- --------------------------------------------------------
 
 --
@@ -228,60 +81,6 @@ CREATE TABLE `category_state` (
   `time_line_id` int(11) UNSIGNED NOT NULL,
   `category_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `category_state`
---
-
-INSERT INTO `category_state` (`state`, `time_line_id`, `category_id`) VALUES
-('deactivated', 138, 67),
-('deactivated', 138, 68),
-('deactivated', 138, 69),
-('deactivated', 138, 70),
-('deactivated', 139, 67),
-('deactivated', 139, 68),
-('deactivated', 139, 69),
-('deactivated', 139, 70),
-('deactivated', 139, 74),
-('removed', 140, 67),
-('active', 140, 74),
-('active', 140, 68),
-('active', 140, 77),
-('active', 140, 78),
-('active', 140, 83),
-('active', 140, 86),
-('active', 140, 87),
-('active', 140, 88),
-('active', 141, 89),
-('active', 141, 90),
-('active', 141, 91),
-('active', 141, 92),
-('active', 141, 93),
-('active', 141, 96),
-('active', 141, 99),
-('active', 141, 100),
-('active', 141, 101),
-('deactivated', 145, 107),
-('deactivated', 145, 108),
-('deactivated', 145, 109),
-('deactivated', 145, 110),
-('active', 148, 111),
-('active', 149, 107),
-('active', 149, 108),
-('active', 149, 109),
-('active', 149, 110),
-('deactivated', 150, 112),
-('deactivated', 150, 113),
-('deactivated', 150, 114),
-('deactivated', 151, 112),
-('deactivated', 151, 113),
-('deactivated', 151, 114),
-('active', 152, 112),
-('active', 152, 113),
-('active', 153, 115),
-('active', 154, 116),
-('active', 154, 117),
-('active', 154, 118);
 
 -- --------------------------------------------------------
 
@@ -316,24 +115,6 @@ CREATE TABLE `friends` (
   `friend_user_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `friends`
---
-
-INSERT INTO `friends` (`friend_id`, `status`, `own_user_id`, `friend_user_id`) VALUES
-(1, 'friend', 49, 33),
-(2, 'friend', 33, 49),
-(3, 'friend', 48, 33),
-(4, 'friend', 33, 48),
-(5, 'friend', 60, 48),
-(6, 'friend', 48, 60),
-(7, 'friend', 61, 60),
-(8, 'friend', 60, 61),
-(9, 'friend', 50, 33),
-(10, 'friend', 33, 50),
-(11, 'friend', 50, 60),
-(12, 'friend', 60, 50);
-
 -- --------------------------------------------------------
 
 --
@@ -363,22 +144,6 @@ CREATE TABLE `friend_request` (
   `requestee` int(11) UNSIGNED NOT NULL,
   `state` enum('waiting','accepted','ignored','denied') NOT NULL DEFAULT 'waiting'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `friend_request`
---
-
-INSERT INTO `friend_request` (`friend_request_id`, `requester`, `requestee`, `state`) VALUES
-(1060, 33, 50, 'accepted'),
-(1061, 33, 49, 'accepted'),
-(1116, 33, 48, 'accepted'),
-(1117, 48, 50, 'waiting'),
-(1118, 48, 49, 'waiting'),
-(1119, 48, 60, 'accepted'),
-(1120, 60, 50, 'accepted'),
-(1121, 60, 58, 'waiting'),
-(1122, 60, 61, 'accepted'),
-(1123, 48, 61, 'waiting');
 
 -- --------------------------------------------------------
 
@@ -412,34 +177,6 @@ CREATE TABLE `income` (
   `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `income`
---
-
-INSERT INTO `income` (`income_id`, `income`, `budget_Instance_ID`, `time_line_id`, `user_id`, `description`) VALUES
-(112, 500.00, 108, 138, 27, ''),
-(113, 500.00, 108, 139, 27, ''),
-(114, 500.00, 108, 140, 27, ''),
-(115, 500.00, 109, 141, 27, ''),
-(116, 20000.59, 110, 142, 27, ''),
-(120, 500.00, 114, 146, 27, 'dfg'),
-(121, 20000.00, 116, 148, 61, 'Salary'),
-(122, 500.00, 116, 148, 61, 'more'),
-(123, 250.00, 116, 148, 61, 'tax return'),
-(126, 10000.00, 113, 145, 60, 'Tester'),
-(128, 500.00, 113, 145, 60, 'tax'),
-(129, 10000.00, 113, 149, 60, 'Tester'),
-(131, 2000.00, 117, 150, 60, 'Salary'),
-(132, 5000.00, 117, 150, 60, 'tax'),
-(133, 2000.00, 117, 151, 60, 'Salary'),
-(135, 2000.00, 117, 152, 60, 'Salary'),
-(136, 10000.00, 118, 153, 60, 'TAX'),
-(137, 20000.00, 118, 153, 60, 'Salary'),
-(138, 500.00, 119, 154, 73, 'Salary'),
-(139, 100000.00, 119, 154, 73, 'test'),
-(140, 6000.00, 120, 155, 73, 'Salary'),
-(141, 500.00, 121, 156, 50, 'test');
-
 -- --------------------------------------------------------
 
 --
@@ -452,18 +189,6 @@ CREATE TABLE `notification` (
   `user_id` int(11) UNSIGNED NOT NULL,
   `state` enum('seen','unseen') NOT NULL DEFAULT 'unseen'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `notification`
---
-
-INSERT INTO `notification` (`notification_id`, `message`, `user_id`, `state`) VALUES
-(1, 'you request has be accepted from your friend catherine gentles', 33, 'seen'),
-(2, 'you request has be accepted from your friend test test', 33, 'unseen'),
-(3, 'you request has be accepted from your friend goat goat', 48, 'seen'),
-(4, 'you request has be accepted from your friend guss guss', 60, 'seen'),
-(5, 'you request has be accepted from your friend rus rus', 33, 'unseen'),
-(6, 'you request has be accepted from your friend rus rus', 60, 'unseen');
 
 -- --------------------------------------------------------
 
@@ -478,20 +203,6 @@ CREATE TABLE `photos` (
   `status` enum('active','deactivated') NOT NULL DEFAULT 'active'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `photos`
---
-
-INSERT INTO `photos` (`photo_id`, `url`, `user_id`, `status`) VALUES
-(7, '../photos/goat60/error.PNG', 60, 'deactivated'),
-(8, '../photos/goat60/goat0191.jpg', 60, 'deactivated'),
-(9, '../photos/rus50/error.PNG', 50, 'active'),
-(10, '../photos/catherine49/error.PNG', 49, 'active'),
-(11, '../photos/dean55/goat0191.jpg', 55, 'active'),
-(12, '../photos/esa33/goat0191.jpg', 33, 'active'),
-(13, '../photos/goat60/frontImage.jpeg', 60, 'active'),
-(14, '../photos/erica68/frontImage.jpeg', 68, 'active');
-
 -- --------------------------------------------------------
 
 --
@@ -503,20 +214,6 @@ CREATE TABLE `preference` (
   `currency_id` int(11) UNSIGNED NOT NULL DEFAULT '1',
   `user_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `preference`
---
-
-INSERT INTO `preference` (`preference_id`, `currency_id`, `user_id`) VALUES
-(1, 2, 55),
-(2, 1, 60),
-(3, 1, 61),
-(4, 1, 68),
-(5, 1, 70),
-(6, 1, 72),
-(7, 1, 73),
-(8, 1, 75);
 
 -- --------------------------------------------------------
 
@@ -547,29 +244,6 @@ CREATE TABLE `time_line` (
   `budget_Instance_ID` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `time_line`
---
-
-INSERT INTO `time_line` (`time_line_id`, `duration_start`, `duration_end`, `frequency`, `state`, `reset_day`, `budget_Instance_ID`) VALUES
-(138, '2016-01-01', '2016-02-01', 'monthly', 'deactivated', 1, 108),
-(139, '2016-12-01', '2017-01-01', 'monthly', 'deactivated', 1, 108),
-(140, '2017-01-01', '2017-02-01', 'monthly', 'active', 1, 108),
-(141, '2017-01-11', '2017-02-11', 'monthly', 'active', 11, 109),
-(142, '2017-01-12', '2017-02-12', 'monthly', 'active', 12, 110),
-(145, '2017-01-13', '2017-02-13', 'monthly', 'deactivated', 13, 113),
-(146, '2017-01-13', '2017-02-13', 'monthly', 'active', 13, 114),
-(147, '2017-01-01', '2017-02-01', 'monthly', 'active', 1, 115),
-(148, '2017-01-13', '2017-02-13', 'monthly', 'active', 13, 116),
-(149, '2017-06-13', '2017-07-13', 'monthly', 'active', 13, 113),
-(150, '2017-01-14', '2017-02-14', 'monthly', 'deactivated', 14, 117),
-(151, '2017-06-14', '2017-07-14', 'monthly', 'deactivated', 14, 117),
-(152, '2017-08-14', '2017-09-14', 'monthly', 'active', 14, 117),
-(153, '2017-01-19', '2017-02-19', 'monthly', 'active', 19, 118),
-(154, '2017-01-04', '2017-02-04', 'monthly', 'active', 4, 119),
-(155, '2017-01-25', '2017-02-25', 'monthly', 'active', 25, 120),
-(156, '2017-01-25', '2017-02-25', 'monthly', 'active', 25, 121);
-
 -- --------------------------------------------------------
 
 --
@@ -586,29 +260,6 @@ CREATE TABLE `user` (
   `gender` enum('male','female') NOT NULL DEFAULT 'male',
   `role` enum('strd','admin') NOT NULL DEFAULT 'strd'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `firstname`, `last_name`, `email`, `time_stamp`, `password`, `gender`, `role`) VALUES
-(27, 'admin', 'admin', 'admin@admin.com', '2016-11-25 12:49:16', 'JERekfOpfyKW/mGeJ1v5ow==', 'male', 'strd'),
-(33, 'esa', 'esa', 'esa@esa.com', '2016-11-27 04:20:55', 'ZixW50M9tw4kBTwNg1AfEA==', 'male', 'strd'),
-(48, 'test', 'test', 'test@test.com', '2016-11-28 10:29:56', 'MyW7kXjExZ8LWHO1r3QaRw==', 'male', 'admin'),
-(49, 'catherine', 'gentles', 'cath@gmail.com', '2016-12-02 19:28:59', 'bU2cDTb08dWmbHW4W4bV2Q==', 'female', 'strd'),
-(50, 'rus', 'rus', 'rus@rus.com', '2016-12-07 07:24:45', 'VLwgQpX81XLfhv06htKy8w==', 'male', 'strd'),
-(52, 'adolf', 'adolf', 'adolf@adolf.com', '2016-12-07 12:30:45', 'UJFpC7HYzBSBsO+VBNT4Fw==', 'male', 'strd'),
-(53, 'min', 'min', 'min@min.com', '2017-01-06 07:31:19', 'WkZxTkl2yhF/dKcN40mzkg==', 'male', 'strd'),
-(55, 'dean', 'dean', 'dean@dean.com', '2017-01-12 11:16:36', 'PF+5+uHduQ+rKt5uG/iNIQ==', 'male', 'strd'),
-(58, 'Duke', 'Duke', 'duke@duke.com', '2017-01-12 18:03:52', 'J5K2qUC8zf9t2J9rXGvmWA==', 'male', 'strd'),
-(59, 'dash', 'dash', 'dash@dash.com', '2017-01-12 18:05:07', 'AeGd7zvyaVs3MtLz9NAGAA==', 'male', 'strd'),
-(60, 'goat', 'goat', 'goat@goat.com', '2017-01-12 18:08:07', 'aFxZoJ0Qs/VLrB2oXb4Aaw==', 'male', 'strd'),
-(61, 'guss', 'guss', 'guss@guss.com', '2017-01-13 15:30:01', 's+wDM/z2pgRqWdt9n+jKPA==', 'male', 'strd'),
-(68, 'erica', 'erica', 'erica@erica.com', '2017-01-23 13:48:21', 'a1vr7jMM8fpuTQwKkdoywQ==', 'female', 'strd'),
-(70, 'james', 'james', 'james@james.com', '2017-01-23 14:58:17', 'NELyUkvh1ttKgabYPSwoRA==', 'male', 'strd'),
-(72, 'sams', 'sams', 'sams@sams.com', '2017-01-23 15:00:54', 's1SDZmTa4uTuUC3E0f5TvA==', 'male', 'strd'),
-(73, 'jenny', 'jenny', 'jenny@jenny.com', '2017-01-23 15:09:28', 'yiJUqZkh30FJJfpjuiBwMg==', 'female', 'strd'),
-(75, 'jennys', 'jenny', 'jenny@jenny.coms', '2017-01-23 15:11:12', 'yiJUqZkh30FJJfpjuiBwMg==', 'male', 'strd');
 
 -- --------------------------------------------------------
 
@@ -633,31 +284,6 @@ CREATE TABLE `user_budget_instance` (
   `budget_Instance_ID` int(11) UNSIGNED NOT NULL,
   `user_id` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user_budget_instance`
---
-
-INSERT INTO `user_budget_instance` (`user_budget_Instance_Id`, `budget_Instance_ID`, `user_id`) VALUES
-(127, 108, 48),
-(128, 109, 49),
-(129, 110, 49),
-(132, 113, 60),
-(133, 114, 48),
-(134, 115, 60),
-(135, 116, 61),
-(136, 117, 60),
-(137, 118, 60),
-(138, 113, 48),
-(139, 115, 48),
-(140, 113, 61),
-(141, 118, 61),
-(142, 117, 61),
-(143, 115, 61),
-(144, 119, 73),
-(145, 120, 73),
-(146, 121, 50),
-(147, 121, 33);
 
 -- --------------------------------------------------------
 
